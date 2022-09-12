5295fd1867d3fdcd5f27f604d025a4f726f10834
Runc2333 <814537405@qq.com>
2022-09-12 16:10:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def main(user, passwd, step):
         return
     
     if step == '':
-        print ("已设置为随机步数（18000-25000）")
-        step = str(random.randint(18000,25000))
+        print ("已设置为随机步数（25000-30000）")
+        step = str(random.randint(25000,30000))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
